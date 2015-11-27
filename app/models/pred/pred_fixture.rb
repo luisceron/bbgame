166db3838262d8d6079c9116bb53eedb13741c73
Luis Ceron <luismarcoceron@gmail.com>
2015-11-27 12:58:18 +0000

    Moving method From Task to Pred::PredFixture

--- app/models/pred/pred_fixture.rb
+++ app/models/pred/pred_fixture.rb
@@ -61,6 +61,39 @@ class Pred::PredFixture < ActiveRecord::Base
     end
   end
 
+
+
+
+
+
+
+
+
+  #----------------------------------------------------------------------------
+  # Task to check if a Fixture is out of date
+  # Params: Nil
+  # Return: Nil
+  #----------------------------------------------------------------------------
+  def self.out_of_date_fixture
+    pred_fixtures = Pred::PredFixture.where(out_of_date_time: false)
+
+    pred_fixtures.includes(:fixture).each do |pred_fixture|
+      if fixture.date == Date.today && fixture.hour.hour == Time.now.in_time_zone.hour
+        if Time.now.in_time_zone.min >= fixture.hour.min
+          pred_fixture.update_attribute(:out_ofdate_time, true)
+        end
+      end
+    end
+  end
+
+
+
+
+
+
+
+
+
   
   #============================================================================
   #   P R I V A T E   M E T H O D S
