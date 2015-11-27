a4d622b5442ceb19fdeb5214c2fab3bb9688b665
Luis Ceron <luismarcoceron@gmail.com>
2015-11-27 13:19:32 +0000

    New approach for method: out_of_date_fixture

--- app/models/pred/pred_fixture.rb
+++ app/models/pred/pred_fixture.rb
@@ -74,10 +74,23 @@ class Pred::PredFixture < ActiveRecord::Base
   # Params: Nil
   # Return: Nil
   #----------------------------------------------------------------------------
+  # def self.out_of_date_fixture
+  #   pred_fixtures = Pred::PredFixture.where(out_of_date_time: false)
+
+  #   pred_fixtures.includes(:fixture).each do |pred_fixture|
+  #     if fixture.date == Date.today && fixture.hour.hour == Time.now.in_time_zone.hour
+  #       if Time.now.in_time_zone.min >= fixture.hour.min
+  #         pred_fixture.update_attribute(:out_ofdate_time, true)
+  #       end
+  #     end
+  #   end
+  # end
+
   def self.out_of_date_fixture
     pred_fixtures = Pred::PredFixture.where(out_of_date_time: false)
 
-    pred_fixtures.includes(:fixture).each do |pred_fixture|
+    pred_fixtures.each do |pred_fixture|
+      fixture = Comp::Fixture.find(pred_fixture.fixture_id)
       if fixture.date == Date.today && fixture.hour.hour == Time.now.in_time_zone.hour
         if Time.now.in_time_zone.min >= fixture.hour.min
           pred_fixture.update_attribute(:out_ofdate_time, true)
@@ -85,7 +98,6 @@ class Pred::PredFixture < ActiveRecord::Base
       end
     end
   end
-
 
 
 
